d147620190c6e43b1e48890fb3f30be602daa320
NNeshz <neftaligow01@gmail.com>
2025-04-16 01:25:27 -0600

    refactor: 💡 Better Auth Plugin
    Refactor the better auth plugin and using a new route for examples

---
+++ packages/api/src/utils/betteAuthPlugin.ts
@@ -0,0 +1,21 @@
+import { Elysia } from "elysia";
+import { auth } from "@camaras/auth";
+
+export const betterAuth = new Elysia({ name: "better-auth" })
+  .mount(auth.handler)
+  .macro({
+    auth: {
+      async resolve({ error, request: { headers } }) {
+        const session = await auth.api.getSession({
+          headers,
+        });
+
+        if (!session) return error(401);
+
+        return {
+          user: session.user,
+          session: session.session,
+        };
+      },
+    },
+  });
