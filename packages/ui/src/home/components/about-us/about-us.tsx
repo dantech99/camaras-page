917388bd3517457a03a06e8073112cb89c1b1966
Miguel Andres Valenzuela <remandres96@hotmail.com>
2025-04-17 11:43:51 -0500

    REFACTOR: change size pictures test

--- packages/ui/src/home/components/about-us/about-us.tsx
+++ packages/ui/src/home/components/about-us/about-us.tsx
@@ -42,7 +42,7 @@ export function AboutSection() {
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
-                  transition={{ duration: 0.8 }}
+                  transition={{ duration: 0.3 }}
                   className="aspect-[1/1] w-full"
                 >
                   <img
