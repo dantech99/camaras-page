3152b33c43f5c8f660c48794001c8d3f98d30edc
Neftali Hernandez <neftaligow01@gmail.com>
2025-04-22 17:00:06 -0600

    🎸 feat: Create the sidebar for the dashboard
    - Install different components from shadcn
    - Impement a navbar and sidebar

---
+++ apps/web/src/modules/dashboard/sidebar/dashboard-navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@camaras/ui/src/components/breadcrumb";
+
+import { SidebarTrigger } from "@camaras/ui/src/components/sidebar";
+import { Separator } from "@camaras/ui/src/components/separator";
+
+export function DashboardNavbar() {
+    return (
+        <header className="flex shrink-0 items-center gap-2 p-2">
+            <div className="flex items-center gap-2 px-1">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem className="hidden md:block">
+                            <BreadcrumbLink href="#">
+                                Building Your Application
+                            </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator className="hidden md:block" />
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </div>
+        </header>
+    )
+}
