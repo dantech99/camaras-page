d2ba2b4e024943feb67bbc29f33bce7a7c685a2d
Neftali Hernandez <neftaligow01@gmail.com>
2025-04-23 11:25:47 -0600

    🎸 feat: Implement responsive package creation form with dialog and drawer components
    - Implement a route to handle the packages
    - Create and get all packages

--- packages/api/src/modules/packages-photos/packager-photos.service.ts
+++ packages/api/src/modules/packages-photos/packager-photos.service.ts
@@ -1,8 +1,16 @@
 import { prisma } from "../prisma";
 
 export class PackageService {
-  async createPackage(data: { name: string, description: string, price: number, photos: string[] }, user: { id: string }) {
-    const { name, description, price, photos } = data
+  async createPackage(
+    data: {
+      name: string;
+      description: string;
+      price: number;
+      photos: string[];
+    },
+    user: { id: string }
+  ) {
+    const { name, description, price, photos } = data;
 
     const photoPackage = await prisma.photoPackage.create({
       data: {
@@ -13,13 +21,27 @@ export class PackageService {
         photographerId: user.id,
         isActive: true,
         discountPercentage: 0,
-      }
-    })
+      },
+    });
 
     return {
       message: "Package created successfully",
       status: 201,
       photoPackage,
-    }
+    };
+  }
+
+  async getPackagesFromPhotographer(photographerId: string) {
+    const packages = await prisma.photoPackage.findMany({
+      where: {
+        photographerId,
+      },
+    });
+
+    return {
+      message: "Packages retrieved successfully",
+      status: 200,
+      packages,
+    };
   }
 }
