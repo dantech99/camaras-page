8ee9a2201a3a0a35196c0e939d176f94dde2a626
Neftali Hernandez <neftaligow01@gmail.com>
2025-04-25 12:58:04 -0600

    fix: Delete unnecesary code

--- apps/web/src/utils/api-connection.ts
+++ apps/web/src/utils/api-connection.ts
@@ -1,6 +1,4 @@
 import type { Api } from "@camaras/api/src";
 import { treaty } from "@elysiajs/eden";
 
-const apiClient = treaty<Api>(process.env.NEXT_PUBLIC_BACKEND_URL).api;
-
-apiClient.photographer.index.get();
+export const apiClient = treaty<Api>(process.env.NEXT_PUBLIC_BACKEND_URL).api;
