6dd18eacacb83bc6f46d9a8bad9ef368938033d4
Miguel Andres Valenzuela <remandres96@hotmail.com>
2025-04-26 14:12:25 -0500

    FIX: Fix component table

--- apps/web/src/modules/dashboard/paquetes/table-paquetes.tsx
+++ apps/web/src/modules/dashboard/paquetes/table-paquetes.tsx
@@ -40,104 +40,6 @@ export function TablePaquetes() {
   }
 
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <Table className="w-full rounded-lg overflow-hidden">
-      <TableHeader className="bg-sidebar-accent w-full">
-        <TableRow className="w-full">
-          <TableHead className="min-w-[60px] text-center">Nombre</TableHead>
-          <TableHead className="min-w-[150px] text-center">
-            Descripción
-          </TableHead>
-          <TableHead className="min-w-[120px] text-center">Precio</TableHead>
-          <TableHead className="min-w-[100px] text-center">
-            Total de fotos
-          </TableHead>
-          <TableHead className="min-w-[120px] text-center">Descuento</TableHead>
-          <TableHead className="min-w-[120px] text-center">
-            Disponible
-          </TableHead>
-          <TableHead className="w-[100px] min-w-[100px] text-center">
-            Acciones
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {isLoading ? (
-          <TableRow>
-            <TableCell colSpan={7} className="text-center py-4">
-              <div className="flex items-center justify-center gap-2">
-                <Loader2 className="h-4 w-4 animate-spin" />
-                <span>Cargando paquetes...</span>
-              </div>
-            </TableCell>
-          </TableRow>
-        ) : isError ? (
-          <TableRow>
-            <TableCell colSpan={7} className="text-center py-6">
-              <div className="text-red-500">
-                Error al cargar los tickets. Por favor, intente nuevamente.
-              </div>
-            </TableCell>
-          </TableRow>
-        ) : packages.length > 0 ? (
-          packages.map((photoPackage: PhotographersPackages) => (
-            <TableRow key={photoPackage.id}>
-              <TableCell className="min-w-[60px] text-left">
-                {photoPackage.name}
-              </TableCell>
-              <TableCell className="min-w-[150px] text-left">
-                {photoPackage.description}
-              </TableCell>
-              <TableCell className="min-w-[120px] text-center">
-                {photoPackage.price}
-              </TableCell>
-              <TableCell className="min-w-[100px] text-center">
-                {photoPackage.photoCount}
-              </TableCell>
-              <TableCell className="min-w-[120px] text-center">
-                {photoPackage.discountPercentage}
-              </TableCell>
-              <TableCell className="min-w-[120px] text-center">
-                {photoPackage.isActive ? (
-                  <span className="text-green-500">Sí</span>
-                ) : (
-                  <span className="text-red-500">No</span>
-                )}
-              </TableCell>
-              <TableCell className="w-[100px] min-w-[100px] text-center">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                      <span className="sr-only">Abrir menu</span>
-                      <MoreHorizontal className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuItem className="cursor-pointer items-center">
-                      Editar
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="cursor-pointer items-center">
-                      Eliminar
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
-          ))
-        ) : (
-          <TableRow>
-            <TableCell colSpan={7} className="text-center py-6">
-              No hay paquetes disponibles.
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-=======
-=======
-
->>>>>>> 06c82e67aed3a593503a0ce5320cbd811ab1d82c
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4">
       {packages.map((pack: PhotographersPackages) => (
         <Card key={pack.id} className="overflow-hidden border rounded-lg shadow-sm py-0">
@@ -174,10 +76,6 @@ export function TablePaquetes() {
         </Card>
       ))}
     </div>
-<<<<<<< HEAD
->>>>>>> 5ee99d079acc8983c4bc0389a60da3ab0c027502
-=======
 
->>>>>>> 06c82e67aed3a593503a0ce5320cbd811ab1d82c
   );
 }
