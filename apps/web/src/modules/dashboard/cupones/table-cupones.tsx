a405f86dc2175848f4220408ddf3c506425bc158
Neftali Hernandez <neftaligow01@gmail.com>
2025-04-27 18:10:07 -0600

    feat: 🎸 Make the discount code work in the front

--- apps/web/src/modules/dashboard/cupones/table-cupones.tsx
+++ apps/web/src/modules/dashboard/cupones/table-cupones.tsx
@@ -1,13 +1,23 @@
+"use client"
+
 import {
   Table,
   TableBody,
+  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@camaras/ui/src/components/table";
+import { useCoupons } from "./hooks/use-cupons";
+import { DeleteIcon, Loader2, SwitchCamera } from "lucide-react";
+import { Button } from "@camaras/ui/src/components/button";
+import { Pencil1Icon } from "@radix-ui/react-icons";
 
 export const TableCoupons = () => {
-  
+
+  const { data, isLoading, isError } = useCoupons()
+  const coupons = data?.coupons || []
+
   return (
     <Table className="w-full rounded-lg overflow-hidden">
       <TableHeader className="bg-sidebar-accent w-full">
@@ -27,7 +37,54 @@ export const TableCoupons = () => {
           <TableHead className="min-w-[100px] text-center">Acciones</TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody></TableBody>
+      <TableBody>
+        {
+          isLoading ? (
+            <TableRow>
+              <TableCell colSpan={5} className="text-center py-4">
+                <div className="flex items-center justify-center gap-2">
+                  <Loader2 className="h-4 w-4 animate-spin" />
+                  <span>Cargando tickets...</span>
+                </div>
+              </TableCell>
+            </TableRow>
+          ) : isError ? (
+            <TableRow>
+              <TableCell colSpan={9} className="text-center py-6">
+                <div className="text-red-500">
+                  Error al cargar los tickets. Por favor, intente nuevamente.
+                </div>
+              </TableCell>
+            </TableRow>
+          ) : coupons.length > 0 ? (
+            coupons.map((coupon) => (
+              <TableRow key={coupon.id} className="text-center">
+                <TableCell>{coupon.photographerId}</TableCell>
+                <TableCell>{coupon.expirationDate ? new Date(coupon.expirationDate).toLocaleDateString() : "N/A"}</TableCell>
+                <TableCell>{coupon.code}</TableCell>
+                <TableCell>{Number(coupon.discountPercentage)}</TableCell>
+                <TableCell className="space-x-2">
+                  <Button>
+                    <SwitchCamera />
+                  </Button>
+                  <Button>
+                    <DeleteIcon />
+                  </Button>
+                  <Button>
+                    <Pencil1Icon />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={9} className="text-center py-6">
+                No hay tickets disponibles
+              </TableCell>
+            </TableRow>
+          )
+        }
+      </TableBody>
     </Table>
   );
 };
