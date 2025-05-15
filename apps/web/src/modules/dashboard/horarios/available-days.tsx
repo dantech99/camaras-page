924b69dc5855252dc77fe6748ec83d07119a87be
Neftali Hernandez <neftaligow01@gmail.com>
2025-05-15 10:19:21 -0600

    Algo para el json

--- apps/web/src/modules/dashboard/horarios/available-days.tsx
+++ apps/web/src/modules/dashboard/horarios/available-days.tsx
@@ -2,6 +2,9 @@
 
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@camaras/ui/src/components/accordion";
 import { useFSessions } from "@/hooks/use-fsessions";
+import { Button } from "@camaras/ui/src/components/button";
+import { format } from "date-fns";
+import { Input } from "@camaras/ui/src/components/input";
 
 export function AvailableDays() {
     const { data: fsessions, isLoading } = useFSessions();
@@ -12,22 +15,26 @@ export function AvailableDays() {
 
     return (
         <div>
-            <h2>Días que elegiste:</h2>
             <Accordion type="single">
                 {fsessions?.map((fsession) => (
                     <AccordionItem key={fsession.id} value={fsession.id}>
                         <AccordionTrigger>
                             <div className="flex items-center justify-between">
-                                <h3>{fsession.date}</h3>
-                                <p>{fsession.isAvailable ? 'Disponible' : 'No disponible'}</p>
+                                <h3>{format(new Date(fsession.date), "yyyy/MM/dd")}</h3>
                             </div>
                         </AccordionTrigger>
                         <AccordionContent>
-                            <div>
+                            <div className="flex flex-col gap-2">
                                 {fsession.timeSlots.map((slot) => (
-                                    <div key={slot.id}>
-                                        <p>{slot.start}</p>
-                                        <p>{slot.end}</p>
+                                    <div key={slot.id} className="flex items-center justify-between gap-1">
+                                        <div className="flex gap-1">
+                                            <Input defaultValue={slot.start ?? ''} type="time" className="w-48" />
+                                            <Input defaultValue={slot.end ?? ''} type="time" className="w-48" />
+                                        </div>
+                                        <div className="flex gap-1">
+                                            <Button variant="outline">Eliminar</Button>
+                                            <Button variant="outline">Editar</Button>
+                                        </div>
                                     </div>
                                 ))}
                             </div>
